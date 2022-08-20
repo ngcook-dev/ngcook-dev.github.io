3a1459e907f081043374fb114536094d6f59535e
ngcook-dev <nick@ngcook.com>
2022-08-20 16:44:40 +1000

    Slider testing

--- script.js
+++ script.js
@@ -3,3 +3,13 @@
 $(function(){
     alert('Hello?');
 })
+
+$(document).on('ready', function() {
+    $(".vertical-center-4").slick({
+      dots: true,
+      vertical: true,
+      centerMode: true,
+      slidesToShow: 4,
+      slidesToScroll: 2
+    });
+})
